1692a29a03cbde26d098ee27c658205ee08ff4f7
Garima Chhikara <garimachhikara128@gmail.com>
2018-09-08 22:15:09 +0530

    Array Ques

--- src/L4_Sep3/ArrayOps.java
+++ src/L4_Sep3/ArrayOps.java
@@ -15,12 +15,20 @@ public class ArrayOps {
 	public static void main(String[] args) {
 
 		// int[] array = takeInput();
-		int[] array = { 10, 20, 30, 50, 80 };
+		int[] array = { 1000, 200, 150, 130, 120, 100 };
 		display(array);
 
 		System.out.println(max(array));
 		System.out.println(linearSearch(array, 80));
 		System.out.println(binarySearch(array, 80));
+
+		int[] res = rotate(array, -9);
+		display(res);
+
+		// bubbleSort(array);
+		// selectionSort(array);
+		insertionSort(array);
+		display(array);
 	}
 
 	public static int[] takeInput() {
@@ -98,6 +106,114 @@ public class ArrayOps {
 		}
 
 		return -1;
+
+	}
+
+	public static void reverse(int[] arr) {
+
+		int left = 0;
+		int right = arr.length - 1;
+
+		while (left < right) {
+			// swap
+			int temp = arr[left];
+			arr[left] = arr[right];
+			arr[right] = temp;
+
+			left++;
+			right--;
+		}
+	}
+
+	public static int[] rotate(int[] arr, int r) {
+
+		r = r % arr.length;
+
+		if (r < 0) {
+			r = r + arr.length;
+		}
+
+		int[] ans = new int[arr.length];
+
+		for (int i = 0; i < ans.length; i++) {
+
+			if (i < r) {
+				ans[i] = arr[i + arr.length - r];
+			} else {
+				ans[i] = arr[i - r];
+			}
+		}
+
+		return ans;
+
+	}
+
+	public static int[] inverse(int[] arr) {
+
+		int[] ans = new int[arr.length];
+
+		for (int i = 0; i < ans.length; i++) {
+			ans[arr[i]] = i;
+		}
+
+		return ans;
+
+	}
+
+	public static void bubbleSort(int[] arr) {
+
+		for (int counter = 0; counter < arr.length - 1; counter++) {
+
+			for (int j = 0; j < arr.length - counter - 1; j++) {
+
+				if (arr[j] > arr[j + 1]) {
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+
+			}
+
+		}
+
+	}
+
+	public static void selectionSort(int[] arr) {
+
+		for (int counter = 0; counter < arr.length - 1; counter++) {
+
+			int min = counter;
+
+			for (int j = counter + 1; j <= arr.length - 1; j++) {
+
+				if (arr[j] < arr[min]) {
+					min = j;
+				}
+			}
+
+			// swap
+			int temp = arr[min];
+			arr[min] = arr[counter];
+			arr[counter] = temp;
+		}
+
+	}
+
+	public static void insertionSort(int[] arr) {
+
+		for (int counter = 1; counter < arr.length; counter++) {
+
+			int temp = arr[counter];
+
+			int j = counter - 1;
+
+			while (j >= 0 && arr[j] > temp) {
+				arr[j + 1] = arr[j];
+				j--;
+			}
+
+			arr[j + 1] = temp;
+		}
 
 	}
 
